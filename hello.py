de4aec37059b1d2c9512dfacc8e256926cf8a521
YuWenHao1212 <wenhao.yu@uni.minerva.edu>
2025-06-23 20:32:33 +0800

    優化 hello.py 結構：加入 UTF-8 編碼宣告、docstring、主函式與主程式保護

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
 # hello.py
-# 這是一個簡單的 Python 程式，會輸出 Hello, world!
-print("Hello, world!") 
+"""
+一個簡單的 Hello World 程式。
+這個程式展示了 Python 的基本輸出功能。
+"""
+
+def main():
+    """主函數：印出 Hello, world! 訊息"""
+    print("Hello, world!")
+
+if __name__ == '__main__':
+    main() 
